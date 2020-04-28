b952ec4c1e7df51349d60795d6c6162f3871ca65
Dinesh Raturi <dineshraturi22@gmail.com>
2020-04-28 11:07:34 +0530

    you can delete your db also with TesetMongo script

--- sendEmail/TestMongo.py
+++ sendEmail/TestMongo.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://test:test@cluster0-he06i.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://username:password@cluster0-XYZURL.mongodb.net/test?retryWrites=true&w=majority")
 db = client.get_database('covid19DB')
 records = db.chat_records
 print(records.count_documents({}))
